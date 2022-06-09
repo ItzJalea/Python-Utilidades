5d93c20eddd70a0015ced18c46b6971be3264b0c
ImJalea <62162624+ItzJalea@users.noreply.github.com>
2022-06-09 12:10:18 -0400

    Create Nombre con mas de x caracteres.py

---
+++ Nombre con mas de x caracteres.py
@@ -0,0 +1,6 @@
+ombre = ""
+while len(nombre) < 6: #Si el Nombre tiene menos de 6 caracteres se repetira
+    nombre = input("Indique su nombre y apellido: ") #Esto pide el nombre y apellido del usuario
+    if len(nombre) > 6: #Si el nombre tiene mas de 6 caracteres saldra del ciclo
+        break
+    print("Tu nombre y apellido deberia ser mas largo") # En caso de que el nombre no cumpla los requisitos mandara un mensaje
